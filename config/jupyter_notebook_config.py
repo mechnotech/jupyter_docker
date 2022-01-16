ff32b2d935cda5a80b4854591ab44cf21dae12cb
mechnotech <mechnotech@yandex.ru>
2022-01-16 12:09:31 +0300

    first commit

---
+++ config/jupyter_notebook_config.py
@@ -0,0 +1,7 @@
+## The password for HTTP authentication.  (JUPYTER_GATEWAY_HTTP_PWD env var)
+#  Default: None
+c.GatewayClient.http_pwd = 'password'
+
+## The username for HTTP authentication. (JUPYTER_GATEWAY_HTTP_USER env var)
+#  Default: None
+c.GatewayClient.http_user = 'user'
